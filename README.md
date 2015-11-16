7bf2cc066e507161ebf44f9ee3926dc071010759
Eric Nishio <eric@self-learner.com>
2015-11-16 20:06:56 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+instagram-save
+==============
+
+Downloads and saves Instagram photos and videos to your current working
+directory.
+
+## Setup
+
+Requirements: *Node 4+*
+
+```
+npm install -g instagram-save
+```
+
+## Usage
+
+Call `instagram-save` and pass a media ID as an argument, like so:
+
+```
+instagram-save dU4fHDw-Ho
+```
---
+++ index.js
@@ -0,0 +1,76 @@
+const cheerio = require('cheerio');
+const request = require('request');
+const clc = require('cli-color');
+const fs = require('fs');
+const _ = require('lodash');
+const dir = process.cwd();
+const id = _.last(process.argv); // TODO: Add support for multiple IDs.
+
+if (process.argv.length < 3) {
+  return console.log(clc.yellow(`Usage: `) + clc.white(`instagram-save MEDIA_ID`));
+}
+
+save(id);
+
+/**
+ * @param  {string} mediaId
+ * @return {Promise}
+ */
+function save(mediaId) {
+  return new Promise((resolve, reject) => {
+    const url = `https://www.instagram.com/p/${mediaId}/`;
+    const filename = `${dir}/${mediaId}.jpg`;
+
+    request(url, (err, response, body) => {
+      if (err) {
+        console.log(clc.red(`Failed to request resource: ${url}`));
+
+        return reject();
+      }
+
+      const $ = cheerio.load(body);
+      const isVideo = $('meta[name="medium"]').attr('content') === 'video';
+      const mimeType = $('meta[property="og:video:type"]').attr('content') || 'image/jpeg';
+      const filename = createFilename(mediaId, mimeType);
+      const downloadUrl = isVideo
+        ? $('meta[property="og:video"]').attr('content')
+        : $('meta[property="og:image"]').attr('content');
+
+      if (!downloadUrl) {
+        console.log(clc.red(`Invalid media ID `) + clc.white(`${mediaId}`));
+
+        return reject();
+      }
+
+      request.head(downloadUrl, (err, res, body) => {
+        request(downloadUrl)
+          .pipe(fs.createWriteStream(filename))
+          .on('close', () => {
+            const mediaType = isVideo ? 'Video' : 'Photo';
+
+            console.log(clc.green(`${mediaType} saved to `) + clc.white(`${filename}`));
+
+            return resolve(filename);
+          });
+      });
+    });
+  });
+}
+
+/**
+ * @param  {string} mediaId
+ * @param  {string} mimeType
+ * @return {string}
+ */
+function createFilename(mediaId, mimeType) {
+  const mimeTypeToExtensionMap = {
+    'video/mp4': '.mp4',
+    'video/ogg': '.ogg',
+    'video/webm': '.webm',
+    'image/jpeg': '.jpg'
+  };
+
+  const extension = _.get(mimeTypeToExtensionMap, mimeType, '.jpg');
+
+  return `${dir}/${mediaId}${extension}`;
+}
