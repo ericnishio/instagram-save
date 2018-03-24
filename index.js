b414ffce55ede174c535458bdf6650e456cf98dd
MohGanji <mohammad-ganji@outlook.com>
2018-03-24 23:18:37 +0430

    caption added to result, contains a small portion of caption

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const createUrl = require('./lib/create-url');
-const downloadAndSave = require('./lib/download-and-save');
-const parsePage = require('./lib/parse-page');
+const createUrl = require("./lib/create-url");
+const downloadAndSave = require("./lib/download-and-save");
+const parsePage = require("./lib/parse-page");
 
 module.exports = save;
 
@@ -18,14 +18,16 @@ function save(urlOrMediaId, dir) {
         const filename = post.filename;
         const isVideo = post.isVideo;
         const mimeType = post.mimeType;
+        const caption = post.caption;
         const file = `${dir}/${filename}`;
 
         downloadAndSave(downloadUrl, file).then(() => {
           return resolve({
             file,
+            caption,
             mimeType,
             url,
-            label: isVideo ? 'video' : 'photo',
+            label: isVideo ? "video" : "photo",
             source: downloadUrl
           });
         });
---
+++ test.js
@@ -0,0 +1,5 @@
+const save = require("./");
+
+save("BVWLb7Vh5EH", "./")
+  .then(console.log)
+  .catch(console.log);
